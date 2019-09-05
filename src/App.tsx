8663b243172cea473db2277f0d7db7240177abca
mtgto <hogerappa@gmail.com>
2019-09-05 21:27:59 +0900

    Add two buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import './App.css';
 import TweetButton from './TweetButton';
 
 const App: React.FC = () => {
-  const [visibleTweetButton, setVisibleTweetButton] = useState(true);
+  const [visibleTweetButtonByLoad, setVisibleTweetButtonByLoad] = useState(true);
+  const [visibleTweetButtonByCreateShareButton, setVisibleTweetButtonByCreateShareButton] = useState(true);
   return (
     <div className="App">
-      {visibleTweetButton && <TweetButton text=""/>}
-      <button onClick={() => setVisibleTweetButton(!visibleTweetButton)}>{visibleTweetButton ? "Tweetボタンを非表示" : "Tweetボタンを表示"}</button>
+      <div>
+        {visibleTweetButtonByLoad && <TweetButton text="" useLoad/>}
+        <button onClick={() => setVisibleTweetButtonByLoad(!visibleTweetButtonByLoad)}>{visibleTweetButtonByLoad ? "Tweetボタンを非表示 (load)" : "Tweetボタンを表示 (load)"}</button>
+      </div>
+      <div>
+        {visibleTweetButtonByCreateShareButton && <TweetButton text="" useLoad={false}/>}
+        <button onClick={() => setVisibleTweetButtonByCreateShareButton(!visibleTweetButtonByCreateShareButton)}>{visibleTweetButtonByCreateShareButton ? "Tweetボタンを非表示 (createShareButton)" : "Tweetボタンを表示 (createShareButton)"}</button>
+      </div>
     </div>
   );
 }
--- src/TweetButton.tsx
+++ src/TweetButton.tsx
@@ -1,8 +1,9 @@
-"use strict";
-
 import React from "react";
 
-type Props = { text: string };
+type Props = {
+  readonly useLoad: boolean; // twttr.widgets.load を使うかどうか
+  readonly text: string;
+};
 
 declare const twttr: TwitterWidgets;
 
@@ -15,28 +16,30 @@ type TwitterWidgets = {
 };
 
 /**
- * Tweetボタン. twttr.widgets.load を使う場合、コンポーネントが消えるときに以下のエラーが発生します.
+ * Tweetボタン. twttr.widgets.loadを使う版とtwttr.widgets.createShareButtonを使う版があります.
+ * twttr.widgets.load を使う場合、コンポーネントが消えるときに以下のエラーが発生します.
  * react-dom.development.js:10557 Uncaught DOMException: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.
  */
 export default class TweetButton extends React.Component<Props> {
   componentDidMount() {
-    // twttr.widgets.load版
-    twttr.ready((twttr: TwitterWidgets) => twttr.widgets.load(this.refs.tweetButton));
-
-    // twttr.widgets.createShareButton版
-    //twttr.ready((twttr: TwitterWidgets) => twttr.widgets.createShareButton("https://twitter.com/share", this.refs.tweetButton, {}));
+    if (this.props.useLoad) {
+      twttr.ready((twttr: TwitterWidgets) => twttr.widgets.load(this.refs.tweetButton));
+    } else {
+      twttr.ready((twttr: TwitterWidgets) => twttr.widgets.createShareButton("https://twitter.com/share", this.refs.tweetButton, {}));
+    }
   }
 
   render() {
-    // twttr.widgets.load版
-    return (
-      <a ref="tweetButton"
-        href="https://twitter.com/share"
-        className="twitter-share-button"
-        data-text={this.props.text}
-        data-show-count="false">Tweet</a>
-    );
-    // // twttr.widgets.createShareButton版
-    //return <div ref="tweetButton" />;
+    if (this.props.useLoad) {
+      return (
+        <a ref="tweetButton"
+          href="https://twitter.com/share"
+          className="twitter-share-button"
+          data-text={this.props.text}
+          data-show-count="false">Tweet</a>
+      );
+    } else {
+      return <div ref="tweetButton" />;
+    }
   }
 }
